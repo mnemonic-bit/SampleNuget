8ee062bfecff30a13fb98840b1b2e5a8c4ea78f8
Marco Ruether <mruether@mnemonic-bit.com>
2024-03-15 16:58:31 +0100

    [main] 'added platform depended code as demo'

--- SampleNuget/Pingable.cs
+++ SampleNuget/Pingable.cs
@@ -5,6 +5,30 @@
     /// </summary>
     public sealed class Pingable
     {
+
+        /// <summary>
+        /// Returns the platform this library is referenced from.
+        /// </summary>
+        public string PlatformName
+        {
+            get
+            {
+#if NET8_0
+                return ".NET 8";
+#elif NET6_0
+                return ".NET 6";
+#elif NETSTANDARD2_0
+                return ".NET Standard";
+#else
+                return "sdf";
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Returns true as kind of alive-sign.
+        /// </summary>
+        /// <returns></returns>
         public bool Pong()
         {
             return true;
